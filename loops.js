d1ae53fec5ed4d796c930ba0da9cd15de14c7b54
Conlin McManus <conlinmcmanus@gmail.com>
2017-01-20 23:30:55 +0000

    Old code that is not working

--- loops.js
+++ loops.js
@@ -1,31 +1,27 @@
 function forLoop() {
-  return Math.random() >= 0.5
-}
 var array = []
 for (var i = 0; i < 25; i++) {
   if (i === 0) {
     array.push("This is 1 strange loop")
-    console.log(array);
-    break;
   } else {
     array = array.push("This is " + i + " strange loops")
     continue;
   }
+  return array
 }
 
 function whileLoop() {
-  return Math.random() >= 0.5
-}
-  var n = 10
   while (n > 0) {
   console.log(--n || "done")
 }
 
 function doWhileLoop() {
-  return Math.random() >= 0.5
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+    return array
 }
-var array = []
-var l = array.length
-do {
-  console.log(l - 1)
-} while (l > 0 || doWhileLoop());
